5fdc99a13aae956e0528750f25044bbda96b6ba2
Brajagopal Mukherjee <106516823+brajagopal21@users.noreply.github.com>
2023-12-14 04:30:02 +0000

    Updates

---
+++ precache-manifest.0a295d508ec624c0444a137892615405.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c34f9424f1bc23838ebaa3a7701a644",
+    "url": "/react-frontend-dev-portfolio/index.html"
+  },
+  {
+    "revision": "777a583a0c1313530e30",
+    "url": "/react-frontend-dev-portfolio/static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "2964b435592438e4ba30",
+    "url": "/react-frontend-dev-portfolio/static/css/main.db6f0745.chunk.css"
+  },
+  {
+    "revision": "777a583a0c1313530e30",
+    "url": "/react-frontend-dev-portfolio/static/js/2.8eb6d666.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/react-frontend-dev-portfolio/static/js/2.8eb6d666.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2964b435592438e4ba30",
+    "url": "/react-frontend-dev-portfolio/static/js/main.720f1d77.chunk.js"
+  },
+  {
+    "revision": "8b91e79b160a90e30d4f",
+    "url": "/react-frontend-dev-portfolio/static/js/runtime-main.0af024f6.js"
+  }
+]);
